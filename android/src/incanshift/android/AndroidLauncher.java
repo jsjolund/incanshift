241ed473b04d32293f21e4c0c70baa204e3024ab
Johannes Sjölund <j.sjolund@gmail.com>
2015-07-01 15:35:15 +0200

    Added initial libGDX files

---
+++ android/src/incanshift/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package incanshift.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import incanshift.IncanShift;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new IncanShift(), config);
+	}
+}
---
+++ desktop/src/incanshift/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package incanshift.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import incanshift.IncanShift;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IncanShift(), config);
+	}
+}
