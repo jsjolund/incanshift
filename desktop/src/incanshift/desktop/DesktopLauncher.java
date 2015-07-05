7340debf9a249a04c56e75cb2b32420cfe94d305
Johannes Sjölund <j.sjolund@gmail.com>
2015-07-05 05:41:05 +0200

    Auto-format

--- desktop/src/incanshift/desktop/DesktopLauncher.java
+++ desktop/src/incanshift/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import incanshift.IncanShift;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new IncanShift(), config);
 	}
