bd5dc187a90a97520c3344bfbe837c99c215fb0b
Johannes Sjölund <j.sjolund@gmail.com>
2015-08-01 14:29:22 +0200

    Moved billboard text to string

--- core/src/incanshift/gameobjects/TextParser.java
+++ core/src/incanshift/gameobjects/TextParser.java
@@ -7,9 +7,68 @@ import com.badlogic.gdx.utils.StringBuilder;
 
 public class TextParser {
 
+	public static String text = "#intro.0\n"+
+			"”We, the human race, will be known through out time by the traces that we leave behind.\"\n"+
+			"- Dakota\n"+
+			"#intro.1\n"+
+			"\"How the children of tomorrow will interpenetrate these traces however, is beyond our comprehension.\"\n"+
+			"- Christoffer Lundberg\n"+
+			"\n"+
+			"#well.0                   \n"+
+			"Gold coins.\n"+
+			"#well.1\n"+
+			"Human bones.\n"+
+			"#well.2     \n"+
+			"Incense.\n"+
+			"#well.3     \n"+
+			"Jade statue.\n"+
+			"#well.4     \n"+
+			"Everything offers to the gods.\n"+
+			"\n"+
+			"#\uFEFFchiuateteo.0\n"+
+			"You are no god, and yet, I recognize your face... Have you been... resurrected?\n"+
+			"#\uFEFFchiuateteo.1\n"+
+			"You, who used to be of such great service to the jaguar god... what could possibly have killed you?\n"+
+			"#\uFEFFchiuateteo.2\n"+
+			"We, the Chiuateteo, are messengers from the kingdom of death, and servants of Tetzcatlipoca, the Jaguar god, to whom this holy site is dedicated.\n"+
+			"#\uFEFFchiuateteo.3\n"+
+			"Why did you return here?\n"+
+			"#\uFEFFchiuateteo.4\n"+
+			"A man that have resurrected must be a powerful man. Tetzcatlipoca might give you a second chance.\n"+
+			"#\uFEFFchiuateteo.5\n"+
+			"All the people have disappeared. Poof! Gone! Just like that! And without man, there is no one to speak the names of the gods.\n"+
+			"#\uFEFFchiuateteo.6\n"+
+			"You appear to be lost.\n"+
+			"#\uFEFFchiuateteo.7\n"+
+			"The rivalry between Tetzcatlipoca and Quetzcoatl is finally at its end.\n"+
+			"\n"+
+			"#player.0\n"+
+			"Where am I? What is this place?\n"+
+			"#player.1\n"+
+			"What god does this land belong to?\n"+
+			"#player.2\n"+
+			"This whole place feels strangely familiar. Yet... so different.\n"+
+			"#player.3\n"+
+			"That mask... I feel like I have used it before. In some kind of ritual or maybe... was it a different mask?\n"+
+			"#player.4\n"+
+			"Was I... a servant of the jaguar god?\n"+
+			"\n"+
+			"#glyphs.0\n"+
+			"Ever since the creation of the worlds, a persistent rivalry between Quetzcoatl and Tezcatlipoca has layed as the foundation for many conflicts of man.\n"+
+			"#glyphs.1\n"+
+			"Some blue masks are symbols of the jaguar god, Tezcatlipoca. Others are of the feathered serpent, Quetzcoatl.\n"+
+			"\n"+
+			"#glyphs.2\n"+
+			"To enter his sanctuary, Kukulcan requires a sacrifice of blood. We praise him with our blood. We honor him with our blood. We give thanks to him with our blood! To him, the feathered serpent, Quetzcoatl, Kukulcan, to our master we willingly give our blood!\n"+
+			"\n"+
+			"#gods.0\n"+
+			"\n"+
+			"#temp.0\n"+
+			"Learn to see the differance between the masks, and you may change the destiny of all mankind for eternity.";
+
 	public static ArrayMap<String, Array<String>> parse(FileHandle textFile) {
 		ArrayMap<String, Array<String>> textMap = new ArrayMap<String, Array<String>>();
-		String text = textFile.readString();
+//		String text = textFile.readString();
 		String[] sections = text.split("#");
 
 		for (int i = 0; i < sections.length; i++) {
